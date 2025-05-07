5162bcca184c2b4063152d16fbe1acf3bb5202de
Kumar Saurabh <saurabhkumar350@gmail.com>
2025-05-07 16:59:27 +0530

    bugs resolve

--- assets/js/lazyYoutube.js
+++ assets/js/lazyYoutube.js
@@ -1,38 +1,15 @@
 window.addEventListener("load", function () {
   const videoContainer = document.querySelector(".video-container");
-  
-  // Create YouTube facade with thumbnail
-  const videoId = "m25a9TmowI8";
   videoContainer.innerHTML = `
-    <div class="youtube-facade" data-video-id="${videoId}">
-      <img 
-        src="https://i.ytimg.com/vi/${videoId}/maxresdefault.jpg" 
-        alt="YouTube Video Thumbnail" 
-        loading="lazy"
-        class="youtube-thumbnail"
-      />
-      <div class="play-button">
-        <svg height="100%" version="1.1" viewBox="0 0 68 48" width="100%">
-          <path class="play-button-bg" d="M66.52,7.74c-0.78-2.93-2.49-5.41-5.42-6.19C55.79,.13,34,0,34,0S12.21,.13,6.9,1.55 C3.97,2.33,2.27,4.81,1.48,7.74C0.06,13.05,0,24,0,24s0.06,10.95,1.48,16.26c0.78,2.93,2.49,5.41,5.42,6.19 C12.21,47.87,34,48,34,48s21.79-0.13,27.1-1.55c2.93-0.78,4.64-3.26,5.42-6.19C67.94,34.95,68,24,68,24S67.94,13.05,66.52,7.74z" fill="#212121" fill-opacity="0.8"></path>
-          <path d="M 45,24 27,14 27,34" fill="#fff"></path>
-        </svg>
-      </div>
-    </div>
+    <iframe
+    id="myVideo"
+      width="100%"
+      height="100%"
+      src="https://www.youtube.com/embed/m25a9TmowI8?autoplay=1&mute=1&controls=0&loop=1&playlist=m25a9TmowI8"
+      title="CYLSYS Office"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
   `;
-  
-  // Add click event listener to load the actual YouTube video
-  document.querySelector(".youtube-facade").addEventListener("click", function() {
-    const videoId = this.getAttribute("data-video-id");
-    this.innerHTML = `
-      <iframe
-        width="100%"
-        height="100%"
-        src="https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1&controls=1"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
-    `;
-  });
 });
